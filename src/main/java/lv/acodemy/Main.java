57c936faf789007a8f887944dd8f1b7ce006b862
D.Benke <torisbeg@gmail.com>
2023-02-22 20:35:47 +0200

    [master]: Upload project [maste]: Add Helllllo

--- src/main/java/lv/acodemy/Main.java
+++ src/main/java/lv/acodemy/Main.java
@@ -1,7 +1,9 @@
 package lv.acodemy;
 
+import java.util.logging.SocketHandler;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello new life!");
-    }
+        System.out.println("HelllllloooO!!!");
 }

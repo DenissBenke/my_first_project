0ce6bc67660fd277348f9f6cdd427fe13ee30262
D.Benke <torisbeg@gmail.com>
2023-03-11 20:24:19 +0200

    [classroom] change version...

---
+++ src/main/java/lv/acodemy/classroom/VariablesDataTypes.java
@@ -0,0 +1,14 @@
+package lv.acodemy.classroom;
+
+public class VariablesDataTypes {
+    public static void main(String[] args) {
+        // byte data type
+        byte myCurrentAge = 36;
+        System.out.println(myCurrentAge);
+
+        // short data type
+        short salaryPerMonth = 1000;
+        System.out.println(salaryPerMonth);
+
+    }
+}
